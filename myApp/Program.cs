87d2df54d2be7b8e1f2b65897f7fbd58186698b8
Jimmy <j.j.notto@gmail.com>
2019-03-27 21:54:00 -0500

    Hello World

---
+++ myApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("The current time is " + DateTime.Now);
+            Console.WriteLine("How do you feel? Y/N");
+            var feeling = Console.ReadLine();
+            switch (feeling)
+            {
+                case "Y": Console.WriteLine("I am unsure of how a person can be doing 'yes', but I support you. :)");
+                break;
+                case "N": Console.WriteLine("You do not want to tell me how you are doing? I only wished to ensure your wellbeing. :(");
+                break;
+                default: Console.WriteLine("Your input is invalid. Respond appropriately or be reprimanded again. >:(");~
+                break;
+            }
+        }
+    }
+}
